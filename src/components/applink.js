19e8229375fc51fb341463c8354f633b2f3bead3
Joe Rupertus <joerup2004@gmail.com>
2024-07-13 10:57:26 -0400

    home changes

--- src/components/applink.js
+++ src/components/applink.js
@@ -5,14 +5,14 @@ import styles from '../styles/utils.module.css'
 const AppLink = ({ app }) => {
   return (
     <Link href={`./${app.string}`}>
-      <div className={styles.applink} style={{ background: `#${app.color}` }}>
+      <div className={styles.applink} style={{ background: `#${app.backgroundColor}` }}>
         <div className={styles.appcontent}>
           <img className={styles.apppreviewimage} src={`images/${app.string}/preview.png`}/> 
           <div>
             <img className={styles.appicon} src={`/images/${app.string}/icon.png`}/> 
-            <h2 className={styles.heading3Xl}>{app.name}</h2>
-            <p className={styles.headingMd}>{app.desc}</p>
-            <p className={styles.learnmore}>Learn more</p>
+            <h2 className={app.theme == 'dark' ? styles.heading3Xl : styles.heading3Xl_light}>{app.name}</h2>
+            <p className={app.theme == 'dark' ? styles.headingMd : styles.headingMd_light}>{app.desc}</p>
+            <p className={app.theme == 'dark' ? styles.learnmore : styles.learnmore_light}>Learn more</p>
           </div>
         </div>
       </div>
